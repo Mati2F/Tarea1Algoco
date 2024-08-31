eb38398ae4274c9e26cdf99dea6ffa5be3255393
Mati2F <matias.fernandezf@usm.cl>
2024-08-30 22:34:20 -0400

    si

--- casosdeprueba.cpp
+++ casosdeprueba.cpp
@@ -3,32 +3,23 @@
 #include <cstdlib>
 #include <ctime>
 
+using namespace std;
+
 int main() {
-    // Inicializar el tamaño n
     int n = 1e7;
+    srand(time(0));
+    ofstream archivo_salida("CasosDePrueba.txt");
 
-    // Inicializar la semilla para números aleatorios
-    std::srand(std::time(0));
-
-    // Crear un archivo de salida
-    std::ofstream archivo_salida("CasosDePrueba.txt");
-
-    // Verificar si el archivo se abrió correctamente
     if (!archivo_salida.is_open()) {
-        std::cerr << "Error al abrir el archivo" << std::endl;
+        cerr << "Error al abrir el archivo" << endl;
         return 1;
     }
 
-    // Generar n números aleatorios y escribirlos en el archivo
     for (int i = 0; i < n; ++i) {
-        int numero_aleatorio = std::rand();
-        archivo_salida << numero_aleatorio << std::endl;
+        int numero_aleatorio = rand();
+        archivo_salida << numero_aleatorio << endl;
     }
-
-    // Cerrar el archivo
     archivo_salida.close();
-
-    std::cout << "Se han generado " << n << " números aleatorios y se han guardado en 'numeros_aleatorios.txt'." << std::endl;
 
     return 0;
 }
